4d8215691890d5f634bd047b67b80a44b1016e66
TONGTONGDESHIJIE <1090060206@qq.com>
2023-04-25 14:50:15 +0800

    feat(custom): 骨架屏优化

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,17 +1,39 @@
 <script setup>
-import { ref } from 'vue'
-import { getStatistics1 } from '../api'
-
 const panels = ref([])
-getStatistics1().then((res) => {
-  panels.value = res
-  console.log(panels.value)
-})
+setTimeout(() => {
+  getStatistics1().then((res) => {
+    panels.value = res
+    console.log(panels.value)
+  })
+}, 2000)
 </script>
 
 <template>
   <div class="p-3">
     <el-row :gutter="20">
+      <template v-if="panels.length == 0">
+        <el-col :span="6" v-for="i in 4" :key="i">
+          <el-skeleton style="width: 100%" animated loading>
+            <template #template>
+              <el-card class="shadow border-0">
+                <template #head>
+                  <div class="f-between">
+                    <el-skeleton-item variant="text" class="w-[50%]"></el-skeleton-item>
+                    <el-skeleton-item variant="text" class="w-[10%]"></el-skeleton-item>
+                  </div>
+                </template>
+                <el-skeleton-item variant="h3" class="w-[80%]" />
+                <el-divider></el-divider>
+                <div class="f-between text-sm text-gray-500">
+                  <el-skeleton-item variant="text" class="w-[50%]" />
+                  <el-skeleton-item variant="text" class="w-[10%]" />
+                </div>
+              </el-card>
+            </template>
+          </el-skeleton>
+        </el-col>
+      </template>
+
       <el-col :span="6" v-for="(item, index) in panels" :key="index">
         <el-card class="shadow hover:(shadow-xl bg-sky-100 opacity-70) cursor-pointer">
           <template #header>
